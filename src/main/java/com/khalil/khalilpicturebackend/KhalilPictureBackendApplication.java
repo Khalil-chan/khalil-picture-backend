a7693369b452e1267882d5e285b8eef09ad3c49f
chenjiajun <khalil_chan@126.com>
2025-12-18 18:33:24 +0800

    init picture backend project!

---
+++ src/main/java/com/khalil/khalilpicturebackend/KhalilPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.khalil.khalilpicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhalilPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhalilPictureBackendApplication.class, args);
+    }
+
+}
