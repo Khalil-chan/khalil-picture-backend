1b49cbdf04a9456a899b1d649182356de7df5c27
chenjiajun <khalil_chan@126.com>
2026-01-04 11:13:16 +0800

    init picture backend project!

--- src/main/java/com/khalil/khalilpicturebackend/KhalilPictureBackendApplication.java
+++ src/main/java/com/khalil/khalilpicturebackend/KhalilPictureBackendApplication.java
@@ -1,9 +1,13 @@
 package com.khalil.khalilpicturebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.khalil.khalilpicturebackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class KhalilPictureBackendApplication {
 
     public static void main(String[] args) {
